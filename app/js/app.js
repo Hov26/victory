84dea75cd0cdc3d0081c1fdaa15381bf4e91f5e6
Hov26 <hov.ghevondyan1994@gmail.com>
2020-12-08 16:27:57 +0400

    add: license section with animation

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,10 @@
-console.log("Hello wolrd");
+$(document).ready(function() {
+    // var topMenu = $("#header-content");
+    var page = $(window);
+    var line = $('.line')
+    page.scroll(function() {
+        if ($(this).scrollTop() > 10) {
+            line.addClass("animate");
+        }
+    });
+})
