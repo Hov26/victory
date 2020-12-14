7ec6408c9eac887c2c75cd1889902e85de5ad4f0
Hov26 <hov.ghevondyan1994@gmail.com>
2020-12-14 14:00:56 +0400

    Add: service page service menu and content web html/css

--- app/js/service-page.js
+++ app/js/service-page.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
+    $('.service-btn').click(function() {
+        var serviceBtn = $(this)
+        if (!$(this).hasClass('active')) {
+            $('.service-btn').removeClass('active')
+            $(this).addClass('active')
+        }
+
+        $(".current-service").each(function() {
+            if (serviceBtn.attr('data-service') === $(this).attr('data-service')) {
+                $(this).addClass('active')
+            } else {
+                $(this).removeClass('active')
+            }
+        });
+    })
+
 
 })
